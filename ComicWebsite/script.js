a3ddc51a0e984af36028682e98bb76b666e88159
ms12297 <ms12297@nyu.edu>
2022-03-03 12:36:20 +0400

    sidebar+libraries added

---
+++ ComicWebsite/script.js
@@ -0,0 +1,5 @@
+ //to toggle sidebar
+ var bars = document.querySelector(".bars");
+    bars.addEventListener("click", function(){
+        document.querySelector("body").classList.toggle("active");
+    })
