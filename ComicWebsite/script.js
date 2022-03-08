511fb36839709db69ba7d589692b6ebad5ae384b
Fatima <faa373@nyu.edu>
2022-03-08 20:30:00 +0400

    added button

--- ComicWebsite/script.js
+++ ComicWebsite/script.js
@@ -59,3 +59,20 @@ var mailFalling = new Howl({
   src:['sounds/mailFalling.mp3']
 });
 
+
+
+//Scroll to top button
+var scrollUp = document.querySelector('.scrollup');
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollUp.style.display = "block";
+  } else {
+    scrollUp.style.display = "none";
+  }
+}
+scrollUp.addEventListener('click', function(){
+  document.documentElement.scrollTop = 0;
+});
