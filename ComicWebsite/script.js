9a2eadf772d9ddccf24631f1e42a5385024ca220
Fatima <faa373@nyu.edu>
2022-03-08 19:19:09 +0400

    added ball sound

--- ComicWebsite/script.js
+++ ComicWebsite/script.js
@@ -11,10 +11,15 @@ window.addEventListener('load', function() { AOS.init(); });
 
 var counter = 0;
 
+var ballSound = new Howl({
+  src:['/Users/fatimaalmaazmi/Desktop/ComLabHW/ComLab-Spring2022-Assignment2/ComicWebsite/sounds/ballSound.mp3']
+});
+
 $(document).ready(function(){
   $(".pan1-front").click(function(){
+    ballSound.play();
     $(".pan1-front").animate({left: "+=40%"}, "slow");
-  });
+    });
   $(".pan1-front").click(function(){
     $(".pan1-front").animate({left: "-=40%"}, "slow");
   });
@@ -31,9 +36,6 @@ var barkBarking = new Howl({
   src:['/Users/fatimaalmaazmi/Desktop/ComLabHW/ComLab-Spring2022-Assignment2/ComicWebsite/sounds/barkBarking.mp3']
 });
 
-var ballSound = new Howl({
-  src:['ComicWebsite/sounds/ballSound.mp3']
-});
 
 var doorClosing = new Howl({
   src:['ComicWebsite/sounds/doorClosing.mp3']
@@ -63,10 +65,10 @@ var mailFalling = new Howl({
 //Sound in Panel 1
 
 // document.querySelector('pan0');
-var playing = false;
-$(window).scroll(function(){
-var pageScroll = $(window).scrollTop();
-if(!playing && pageScroll == 0){
-  barkBarking.play();
-}
-});
+// var playing = false;
+// $(window).scroll(function(){
+// var pageScroll = $(window).scrollTop();
+// if(!playing && pageScroll == 0){
+//   barkBarking.play();
+// }
+// });
